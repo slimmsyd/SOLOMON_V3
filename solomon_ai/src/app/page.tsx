b949765c77176bc1a66165886718099c8c9ce7e9
Sydney Sanders <ssanderss444@gmail.com>
2024-05-29 19:35:41 -0400

    updatd the icon

--- solomon_ai/src/app/page.tsx
+++ solomon_ai/src/app/page.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
 import Login from "./login/page";
-import Signup from "./signup/page"
+import Signup from "./signup/page";
 import Questionaire from "./questionaire/page";
 import { LoadingRender } from "./components/helper/LoadingRender";
 import ChatDashboard from "./chat/app/page";
 import Profile from "./profile/page";
 import HomePage from "./HomePage";
+import { Html, Head, Main, NextScript } from "next/document";
+import Icon from '../app/favicon.ico'
 
 export default function Home() {
   return (
-  
-  <>
+    <>
 
-
-  <HomePage />
-
-
+      <HomePage />
     </>
-  )
+  );
 }
