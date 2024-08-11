1fe581608ff544bc2b696a14c4bc089d12d7d544
Sydney Sanders <ssanderss444@gmail.com>
2024-08-10 20:02:57 -0400

    updatd the home page

--- solomon_ai/src/app/page.tsx
+++ solomon_ai/src/app/page.tsx
@@ -1,10 +1,4 @@
-import Image from "next/image";
-import Login from "./login/page";
-import Signup from "./signup/page";
-import Questionaire from "./questionaire/page";
-import { LoadingRender } from "./components/helper/LoadingRender";
-import ChatDashboard from "./chat/app/page";
-import Profile from "./profile/page";
+
 import HomePage from "./HomePage";
 
 export default function Home() {
